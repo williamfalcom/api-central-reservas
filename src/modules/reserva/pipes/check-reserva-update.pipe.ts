8ee1e67f1754e2c08073b170a440d458bdb3d6c5
William Guimarães <william@wgdev.com.br>
2022-08-02 19:53:19 -0300

    feat(/reserva): atualização de pipe checkReserva

---
+++ src/modules/reserva/pipes/check-reserva-update.pipe.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import * as moment from 'moment';
+
+@Injectable()
+export class CheckReservaUpdatePipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (metadata.type == 'param') {
+      return value;
+    }
+    const dateNow = moment();
+    const dateCheckIn = moment(value.checkIn);
+    const dataCheckOut = moment(value.checkOut);
+    var checkErros = [];
+
+    if (dateCheckIn.diff(dateNow, 'd') < 1) {
+      checkErros.push(
+        'A data de check-id deve estar no mínimo 24 horas no futuro.',
+      );
+    }
+    if (dataCheckOut.diff(dateCheckIn, 'd') < 1) {
+      checkErros.push(
+        'A data de check-out deve ser no minimo 24 horas após o check-in.',
+      );
+    }
+
+    if (checkErros.length > 0) {
+      throw new BadRequestException(checkErros);
+    }
+
+    return value;
+  }
+}
